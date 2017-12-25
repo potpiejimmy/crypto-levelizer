ac14333a8d5614e718647580da621046bb9b492b
potpiejimmy <t.liese@outlook.com>
2017-12-26 00:00:01 +0100

    calculate BTC sum

--- app.ts
+++ app.ts
@@ -6,26 +6,42 @@ console.log("=== The ultimate money machine 0.9 ===\n");
 
 const hmac = crypto.createHmac('sha256', process.env.API_SECRET);
 
-let params = "timestamp=" + Date.now();
-hmac.update(params);
-
-let signature = hmac.digest('hex');
-
-let url = "https://api.binance.com/api/v3/account?" + params + "&signature=" + signature;
-console.log("Reading account assets:");
-console.log("GET " + url);
-
+let prices = {};
 let coinNum = 0;
+let coins = [];
+let totalBtc = 0;
 
-fetch(url, {headers: {"X-MBX-APIKEY": process.env.API_KEY}})
+let url = "https://api.binance.com/api/v1/ticker/allPrices";
+console.log("Reading prices:");
+console.log("GET " + url);
+fetch(url)
 .then(res => res.json())
-.then(res => utils.asyncLoop(res.balances, (i,next) => {
-    let asset = parseFloat(i.free);
-    if (asset > 0 && i.asset !=='BTC') coinNum++;
+.then(allPrices => utils.asyncLoop(allPrices, (i,next) => {
+    prices[i.symbol] = parseFloat(i.price);
     next();
 }))
 .then(() => {
-    console.log("Found " + coinNum + " assets.");
+    let params = "timestamp=" + Date.now();
+    hmac.update(params);
+    url = "https://api.binance.com/api/v3/account?" + params + "&signature=" + hmac.digest('hex');
+    console.log("Reading account assets:");
+    console.log("GET " + url);
+    return fetch(url, {headers: {"X-MBX-APIKEY": process.env.API_KEY}});
+})
+.then(res => res.json())
+.then(res => utils.asyncLoop(res.balances, (i,next) => {
+    let val = parseFloat(i.free);
+    if (val > 0 && i.asset !=='BTC'/* && i.asset !=='BNB'*/) {
+        i.btc = val * prices[i.asset + 'BTC'];
+        console.log(val + " " + i.asset + " * " + prices[i.asset + 'BTC'] + " " + i.asset + "BTC = " + i.btc + " BTC");
+        totalBtc += i.btc;
+        coins.push(i);
+        coinNum++;
+    }
+    next();
+}))
+.then(() => {
+    console.log("Found " + coinNum + " assets with total value " + totalBtc + " BTC.");
 })
 .catch(err => {
     console.log(err);
