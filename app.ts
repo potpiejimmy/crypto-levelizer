bc083e3b1cd52002be019d56cc7efba423689a37
potpiejimmy <t.liese@outlook.com>
2017-12-26 22:08:14 +0100

    initial implementation

--- app.ts
+++ app.ts
@@ -2,47 +2,84 @@ import * as crypto from 'crypto';
 import * as fetch from 'node-fetch';
 import * as utils from './utils';
 
-console.log("=== The ultimate money machine 0.9 ===\n");
+const REF_CUR = 'BNB'; // reference currency for trading
+const IGNORE_LIST = ['BTC']; // currencies to ignore
 
-const hmac = crypto.createHmac('sha256', process.env.API_SECRET);
+console.log("=== The ultimate money machine is running ===\n");
 
-let prices = {};
-let coinNum = 0;
-let coins = [];
-let totalBtc = 0;
+if (!process.env.API_KEY || !process.env.API_SECRET) {
+    console.log("Please set API_KEY and API_SECRET environment variables.");
+    process.exit(1);
+}
 
+let prices = {};  // maps symbols to current prices (float)
+let coins = [];   // current coins to handle
+let totalVal = 0; // total REF_CUR value
+let totalAverage = 0; // average REF_CUR value
+
+// first, get all current prices from the public API
 let url = "https://api.binance.com/api/v1/ticker/allPrices";
-console.log("Reading prices:");
-console.log("GET " + url);
+console.log("Reading prices: GET " + url);
 fetch(url)
 .then(res => res.json())
-.then(allPrices => utils.asyncLoop(allPrices, (i,next) => {
-    prices[i.symbol] = parseFloat(i.price);
-    next();
-}))
-.then(() => {
+.then(allPrices => {
+    // add all prices to our map
+    allPrices.forEach(i => prices[i.symbol] = parseFloat(i.price));
+
+    // now, read all account assets
     let params = "timestamp=" + Date.now();
-    hmac.update(params);
-    url = "https://api.binance.com/api/v3/account?" + params + "&signature=" + hmac.digest('hex');
-    console.log("Reading account assets:");
-    console.log("GET " + url);
+    url = "https://api.binance.com/api/v3/account?" + params + "&signature=" + sign(params);
+    console.log("Reading account assets: GET " + url);
     return fetch(url, {headers: {"X-MBX-APIKEY": process.env.API_KEY}});
 })
 .then(res => res.json())
-.then(res => utils.asyncLoop(res.balances, (i,next) => {
-    let val = parseFloat(i.free);
-    if (val > 0 && i.asset !=='BTC'/* && i.asset !=='BNB'*/) {
-        i.btc = val * prices[i.asset + 'BTC'];
-        console.log(val + " " + i.asset + " * " + prices[i.asset + 'BTC'] + " " + i.asset + "BTC = " + i.btc + " BTC");
-        totalBtc += i.btc;
-        coins.push(i);
-        coinNum++;
-    }
-    next();
-}))
-.then(() => {
-    console.log("Found " + coinNum + " assets with total value " + totalBtc + " BTC.");
+.then(res => {
+    res.balances.forEach(i => {
+        let val = parseFloat(i.free);
+        if (val > 0 && IGNORE_LIST.indexOf(i.asset) == -1) {
+            i.refVal = i.asset === REF_CUR ? val : val * prices[i.asset + REF_CUR];
+            console.log(val + " " + i.asset + " * " + prices[i.asset + REF_CUR] + " " + i.asset + REF_CUR + " = " + i.refVal + " " + REF_CUR);
+            totalVal += i.refVal;
+            coins.push(i);
+        }
+    });
+    console.log("Found " + coins.length + " assets with total value " + totalVal + " " + REF_CUR + ".");
+    totalAverage = totalVal / coins.length;
+    console.log("Now re-trading all coins to match the average of " + totalAverage + " " + REF_CUR + ".");
+    // store diff member (difference to average)
+    coins.forEach(i => i.diff = i.refVal - totalAverage);
+    // sort by diff, descending, so that we are selling to REF_CUR first before buying with REF_CUR
+    coins = coins.sort((a,b) => b.diff - a.diff);
+    // now loop
+    return utils.asyncLoop(coins, (i,next) => {
+        retrade(i)
+            .then(() => setTimeout(() => next(), 500))
+            .catch(err => console.log(err));
+    });
+})
+.then(()=> {
+    console.log("Done.");
 })
 .catch(err => {
     console.log(err);
 });
+
+function retrade(coin) : Promise<any> {
+    if (coin.asset === REF_CUR) return Promise.resolve();
+    let diff = coin.refVal - totalAverage;
+    let absDiff = Math.abs(diff);
+    let quantity: number = absDiff/prices[coin.asset + REF_CUR];
+    let quantityString = quantity.toPrecision(2);
+    let side = (diff > 0 ? 'SELL' : 'BUY');
+    console.log(side + " " + absDiff + " " + REF_CUR + " of " + coin.asset + " = " + quantityString); 
+
+    let params = "symbol=" + coin.asset + REF_CUR + "&side=" + side + "&type=MARKET&quantity=" + quantityString + "&timestamp=" + Date.now();
+    url = "https://api.binance.com/api/v3/order?" + params + "&signature=" + sign(params);
+    return fetch(url, {method:'POST', headers: {"X-MBX-APIKEY": process.env.API_KEY}})
+           .then(res => res.json())
+           .then(res => console.log(res));
+}
+
+function sign(params : string) : string {
+    return crypto.createHmac('sha256', process.env.API_SECRET).update(params).digest('hex');
+}
